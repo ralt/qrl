77fcc83bf7a83b292b96c9de8f821319fe59d5c3
Florian Margaine <florian@margaine.com>
2014-06-15 00:45:57 +0200

    Adds comments to make sure theere is no magic number

--- png.lisp
+++ png.lisp
@@ -3,6 +3,6 @@
 (defun png-draw (pixels pathname size)
   "Draws the pixels on the PNG file."
   (cl-gd:with-image* (size size)
-    (cl-gd:allocate-color 255 255 255)
-    (cl-gd:set-pixels pixels :color (cl-gd:allocate-color 0 0 0))
+    (cl-gd:allocate-color 255 255 255) ; white background
+    (cl-gd:set-pixels pixels :color (cl-gd:allocate-color 0 0 0)) ; black pixels
     (cl-gd:write-image-to-file pathname)))
